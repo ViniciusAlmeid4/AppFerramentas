73b46a91f61ca312e8552cb8aa3d154c42100a9e
Aluno CTM Maringá <alunoctmmaringa@hotmail.com>
2023-09-05 22:10:58 -0300

    criando paginas de visualização

---
+++ AppFerramentas/PageLeitor.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppFerramentas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageLeitor : ContentPage
+    {
+        public PageLeitor()
+        {
+            InitializeComponent();
+        }
+
+        private void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+
+                scanResultText.Text = result.Text + " (type: " + result.BarcodeFormat.ToString() + ")";
+            
+            });
+        }
+    }
+}
---
+++ AppFerramentas/PageVerificacao.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppFerramentas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageVerificacao : ContentPage
+    {
+        public PageVerificacao()
+        {
+            InitializeComponent();
+        }
+    }
+}
